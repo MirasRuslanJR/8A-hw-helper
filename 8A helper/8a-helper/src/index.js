0e7911ca947875a7a2d73f88b6cc77f9ba9051f7
Miras <164633869+MirasRuslanJR@users.noreply.github.com>
2025-04-06 21:27:25 +0500

    l

--- 8A helper/8a-helper/src/index.js
+++ 8A helper/8a-helper/src/index.js
@@ -1,11 +1,11 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './App';  // Импортируй компонент App
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // Рендерим компонент в div с id="root"
 );
